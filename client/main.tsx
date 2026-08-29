aeec527e13e695a726caa3649f143d96efb9b635
Ananthaprakash <ananthaprakashb@gmail.com>
2026-08-29 00:07:52 -0700

    feat: route password recovery screens

--- client/main.tsx
+++ client/main.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppV2 from "./AppV2";
+import { PasswordRecoveryRouter } from "./PasswordRecovery";
 import "./styles.css";
 import "./styles-v2.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppV2 />
+    <PasswordRecoveryRouter app={<AppV2 />} />
   </React.StrictMode>,
 );
