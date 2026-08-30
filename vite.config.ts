9b1bcb2fac314c26f7e58e68952c27ffc41f7086
Ananthaprakash <ananthaprakashb@gmail.com>
2026-08-30 14:30:07 -0700

    fix: version social preview URL at build time

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const SOCIAL_PREVIEW_VERSION = "20260830";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "contextgateway-social-preview-version",
+      transformIndexHtml(html) {
+        return html.replaceAll(
+          "/contextgateway-social.jpg",
+          `/contextgateway-social-${SOCIAL_PREVIEW_VERSION}.jpg`,
+        );
+      },
+    },
+  ],
   build: {
     outDir: "dist",
     emptyOutDir: true,
